d96a242be86d7e4242bdd578f960d9f671954aab
sebpearce <sebpearce@gmail.com>
2017-03-31 20:57:34 +1100

    Create formatAsCurrency function

--- src/helpers/currency-helpers.js
+++ src/helpers/currency-helpers.js
@@ -1,8 +1,75 @@
+import { pipe } from './misc-helpers';
+
+// const stripNonNumericCharacters = x => {
+//   return String(x).replace(/[^0-9]/g, '');
+// };
+
+// const insertDecimal = x => {
+//   return x ? (parseInt(x, 10) / 100).toFixed(2) : '0.00';
+// };
+
+
+
+// export const addPlusSignIfPositive = x => {
+//   const stripped = x.replace(/[^–[0-9].]/g, '');
+//   return Number(stripped) > 0 ? '+'.concat(x) : x;
+// };
+
 export const formatWithCommas = x => {
-  return x.replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+  return String(x).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 };
 
-export const addPlusSignIfPositive = x => {
-  const stripped = x.replace(/[^–[0-9].]/g, '');
-  return Number(stripped) > 0 ? '+'.concat(x) : x;
-};
+const replaceMinusSignWithHyphen = s => {
+  return String(s).replace(/–/, '-');
+}
+
+const makePositive = x => {
+  return Math.abs(x);
+}
+
+const stripNonNumericCharacters = s => {
+  return String(s).replace(/[^0-9-.]/g,'');
+}
+
+const convertToNumber = s => {
+  return Number(s);
+}
+
+const convertToTwoDigitDecimalString = x => {
+  return x.toFixed(2);
+}
+
+const prependMinusSign = x => {
+  return '–'.concat(x);
+}
+
+const prependPlusSign = x => {
+  return '+'.concat(x);
+}
+
+export const determineNumericValue = x => {
+  if (typeof x === 'number') return x;
+  return pipe(
+    x,
+    replaceMinusSignWithHyphen,
+    stripNonNumericCharacters,
+    convertToNumber,
+  )
+}
+
+export const formatAsCurrency = (x, settings) => {
+  const numericValue = determineNumericValue(x);
+  let outputString = pipe(
+    numericValue,
+    makePositive,
+    convertToTwoDigitDecimalString,
+    formatWithCommas,
+  );
+
+  if (settings['plus'] && numericValue > 0) {
+    outputString = prependPlusSign(outputString);
+  } else if (settings['minus'] && numericValue < 0) {
+    outputString = prependMinusSign(outputString);
+  }
+  return outputString;
+}
--- src/helpers/currency-helpers.test.js
+++ src/helpers/currency-helpers.test.js
@@ -17,9 +17,178 @@ describe('formatWithCommas', () => {
   });
 });
 
-describe('addPlusSignIfPositive', () => {
-  it('works', () => {
-    const x = '1234.00';
-    expect(subject.addPlusSignIfPositive(x)).toEqual('+1234.00');
+// describe('addPlusSignIfPositive', () => {
+//   it('works', () => {
+//     const x = '1234.00';
+//     expect(subject.addPlusSignIfPositive(x)).toEqual('+1234.00');
+//   });
+// });
+
+describe('formatAsCurrency', () => {
+  describe('when comma-formatted values are passed in', () => {
+    const settings = {
+      plus: true,
+      minus: true,
+    };
+
+    it('handles negatives', () => {
+      const x = '–1,234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.00');
+    });
+
+    it('handles positives', () => {
+      const x = '1,234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.00');
+    });
+
+    it('handles positives with plus sign', () => {
+      const x = '+1,234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.00');
+    });
+  });
+
+  describe('when numbers are passed in', () => {
+    const settings = {
+      plus: true,
+      minus: true,
+    };
+
+    it('handles negative numbers', () => {
+      const x = -1234;
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.00');
+    });
+
+    it('handles negative one-digit decimals', () => {
+      const x = -1234.5;
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.50');
+    });
+
+    it('handles negative two-digit decimals', () => {
+      const x = -1234.56;
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.56');
+    });
+
+    it('handles positive numbers', () => {
+      const x = 1234;
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.00');
+    });
+
+    it('handles positive one-digit decimals', () => {
+      const x = 1234.5;
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.50');
+    });
+
+    it('handles positive two-digit decimals', () => {
+      const x = 1234.56;
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.56');
+    });
+  });
+
+  describe('when both the plus and minus setting are true', () => {
+    const settings = {
+      plus: true,
+      minus: true,
+    };
+
+    it('handles positives', () => {
+      const x = '1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.00');
+    });
+
+    it('handles negatives', () => {
+      const x = '-1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.00');
+    });
+
+    it('handles positives without a decimal', () => {
+      const x = '1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.00');
+    });
+
+    it('handles negatives without a decimal', () => {
+      const x = '-1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.00');
+    });
+  });
+
+  describe('when neither the plus nor minus settings are true', () => {
+    const settings = {
+      plus: false,
+      minus: false,
+    };
+
+    it('handles positives', () => {
+      const x = '1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+
+    it('handles negatives', () => {
+      const x = '-1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+
+    it('handles positives without a decimal', () => {
+      const x = '1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+
+    it('handles negatives without a decimal', () => {
+      const x = '-1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+  });
+
+  describe("when the plus setting is true but minus isn't", () => {
+    const settings = {
+      plus: true,
+      minus: false,
+    };
+
+    it('handles positives', () => {
+      const x = '1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.00');
+    });
+
+    it('handles negatives', () => {
+      const x = '-1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+
+    it('handles positives without a decimal', () => {
+      const x = '1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('+1,234.00');
+    });
+
+    it('handles negatives without a decimal', () => {
+      const x = '-1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+  });
+
+  describe("when the minus setting is true but plus isn't", () => {
+    const settings = {
+      plus: false,
+      minus: true,
+    };
+
+    it('handles positives', () => {
+      const x = '1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+
+    it('handles negatives', () => {
+      const x = '-1234.00';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.00');
+    });
+
+    it('handles positives without a decimal', () => {
+      const x = '1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('1,234.00');
+    });
+
+    it('handles negatives without a decimal', () => {
+      const x = '-1234';
+      expect(subject.formatAsCurrency(x, settings)).toEqual('–1,234.00');
+    });
   });
 });
