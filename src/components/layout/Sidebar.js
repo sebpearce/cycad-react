d0ec587d34f67ece922907ca7b19223e4644c484
sebpearce <sebpearce@gmail.com>
2017-04-01 16:38:59 +1100

    Add icons to sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Sidebar.css';
+import Icon from '../Icon';
 
 export const Sidebar = () => {
   return (
@@ -8,16 +9,52 @@ export const Sidebar = () => {
       <nav>
         <ul>
           <NavLink exact to="/" activeClassName={styles.selected}>
-            <li className={styles.section}>Capture</li>
+            <li className={styles.section}>
+              <div className={styles.labelAndIcon}>
+                <div className={styles.iconContainer}>
+                  <Icon id="inbox" />
+                </div>
+                <div className={styles.labelContainer}>
+                  Capture
+                </div>
+              </div>
+            </li>
           </NavLink>
           <NavLink to="/overview" activeClassName={styles.selected}>
-            <li className={styles.section}>Overview</li>
+            <li className={styles.section}>
+              <div className={styles.labelAndIcon}>
+                <div className={styles.iconContainer}>
+                  <Icon id="analysis" />
+                </div>
+                <div className={styles.labelContainer}>
+                  Overview
+                </div>
+              </div>
+            </li>
           </NavLink>
           <NavLink to="/budget" activeClassName={styles.selected}>
-            <li className={styles.section}>Budget</li>
+            <li className={styles.section}>
+              <div className={styles.labelAndIcon}>
+                <div className={styles.iconContainer}>
+                  <Icon id="dollar" />
+                </div>
+                <div className={styles.labelContainer}>
+                  Budget
+                </div>
+              </div>
+            </li>
           </NavLink>
           <NavLink to="/settings" activeClassName={styles.selected}>
-            <li className={styles.section}>Settings</li>
+            <li className={styles.section}>
+              <div className={styles.labelAndIcon}>
+                <div className={styles.iconContainer}>
+                  <Icon id="settings" />
+                </div>
+                <div className={styles.labelContainer}>
+                  Settings
+                </div>
+              </div>
+            </li>
           </NavLink>
         </ul>
       </nav>
